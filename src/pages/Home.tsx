de4b799b2a31e746e336e1b81610acd5f48d092d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-29 14:57:11 +0530

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,49 @@
-import Login from "./Login";
-import Register from "./Register";
 
 const Home = () => {
 
-    const salary: number = 10.25;
-    const isLoggedIn = false;
+    const products = [
+        { title: 'Cabbage', id: 1 },
+        { title: 'Garlic', id: 2 },
+        { title: 'Apple', id: 3 },
+    ];
+    console.log(products);
+
+    const listItems = products.map(product =>
+        <li key={product.id}>
+            {product.title}
+        </li>
+    );
     return (
         <>
             <h1>Home Component</h1>
             <p>This is home component.</p>
-            <p>My salary is ₹{salary}/-</p>
-            <> {isLoggedIn && <Login />}
-            </>
-            <> {!isLoggedIn && <Register />}
-            </>
+            {/* <>{products} </> */}
+            <> {listItems} </>
         </>
     );
 };
 export default Home;
+
+// import Login from "./Login";
+// import Register from "./Register";
+
+// const Home = () => {
+
+//     const salary: number = 10.25;
+//     const isLoggedIn = false;
+//     return (
+//         <>
+//             <h1>Home Component</h1>
+//             <p>This is home component.</p>
+//             <p>My salary is ₹{salary}/-</p>
+//             <> {isLoggedIn && <Login />}
+//             </>
+//             <> {!isLoggedIn && <Register />}
+//             </>
+//         </>
+//     );
+// };
+// export default Home;
 
 // import Login from "./Login";
 // import Register from "./Register";
