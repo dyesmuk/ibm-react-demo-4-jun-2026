f47cb59183ab44d9d4078f5c20722b9cb80eae65
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-30 15:21:43 +0530

    initial commit

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -11,14 +11,14 @@ const NavBar = () => {
             <p>{isLoggedIn ? 'yes' : 'no'}</p>
             <nav>
                 <Link to='/home'>Home</Link> |
-                {/* {isLoggedIn && <Link to='/parent'>Parent</Link>}
+                {isLoggedIn && <Link to='/parent'>Parent</Link>}
                 {isLoggedIn && <Link to='/employees'>Employees</Link>}
                 {!isLoggedIn && <Link to='/login'>Login</Link>}
-                {!isLoggedIn && <Link to='/register'>Register</Link>} */}
-                <Link to='/parent'>Parent</Link> |
+                {!isLoggedIn && <Link to='/register'>Register</Link>}
+                {/* <Link to='/parent'>Parent</Link> |
                 <Link to='/login'>Login</Link> |
                 <Link to='/register'>Register</Link> |
-                <Link to='/employees'>Employees</Link>
+                <Link to='/employees'>Employees</Link> */}
             </nav>
         </>
     );
