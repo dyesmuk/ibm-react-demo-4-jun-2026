a28118fcb555f5cd7b0891b798fb423c4d12435c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-30 12:25:29 +0530

    initial commit

---
+++ src/components/navBar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router";
+
+const NavBar = () => {
+
+    return (
+        <>
+            <nav>
+                <Link to='/home'>Home</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+                <Link to='/employees'>Employees</Link>
+            </nav>
+        </>
+    );
+
+};
+
+export default NavBar;
--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import NavBar from "../components/navBar";
 
 const AppRoutes = () => {
 
     return (
         <>
             <BrowserRouter>
+                <NavBar></NavBar>
                 <Routes>
                     <Route path="/home" element={<Home />} />
                     <Route path="/login" element={<Login />} />
