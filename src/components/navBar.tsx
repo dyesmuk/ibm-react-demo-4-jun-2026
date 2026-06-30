f84f79cce977e7e4e9fd5fa45c61dd0272bcc73a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-30 15:16:44 +0530

    initial commit

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -11,6 +11,11 @@ const NavBar = () => {
             <p>{isLoggedIn ? 'yes' : 'no'}</p>
             <nav>
                 <Link to='/home'>Home</Link> |
+                {/* {isLoggedIn && <Link to='/parent'>Parent</Link>}
+                {isLoggedIn && <Link to='/employees'>Employees</Link>}
+                {!isLoggedIn && <Link to='/login'>Login</Link>}
+                {!isLoggedIn && <Link to='/register'>Register</Link>} */}
+                <Link to='/parent'>Parent</Link> |
                 <Link to='/login'>Login</Link> |
                 <Link to='/register'>Register</Link> |
                 <Link to='/employees'>Employees</Link>
--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,90 +1,77 @@
+import { useContext } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+// import Employees from "../pages/Employees";
 import NavBar from "../components/navBar";
 import Page404 from "../pages/Page404";
+import AuthContext from "../context/AuthContextType";
+import Parent from "../pages/Parent";
 
 const AppRoutes = () => {
 
-    return (
-        <>
-            <BrowserRouter>
-                <NavBar></NavBar>
-                <Routes>
-                    <Route path="/" element={<Navigate to="/home" replace />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/employees" element={<Home />} />
-                    <Route path="*" element={<Page404 />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+    const { isLoggedIn } = useContext(AuthContext);
 
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" replace />} />
+                <Route path="/home" element={<Home />} />
+                <Route
+                    path="/login"
+                    element={isLoggedIn ? <Navigate to="/employees" replace /> : <Login />}
+                />
+                <Route
+                    path="/employees" element={isLoggedIn ? <Parent /> : <Navigate to="/login" replace />}
+                />
+                <Route path="*" element={<Page404 />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default AppRoutes;
 
 
-// import { useContext } from "react";
+
+
+
+
 // import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 // import Login from "../pages/Login";
 // import Home from "../pages/Home";
 // import Register from "../pages/Register";
-// // import Employees from "../pages/Employees";
 // import NavBar from "../components/navBar";
 // import Page404 from "../pages/Page404";
-// import AuthContext from "../context/AuthContextType";
-// import Parent from "../pages/Parent";
 
 // const AppRoutes = () => {
 
-//     const { isLoggedIn } = useContext(AuthContext);
-
 //     return (
-//         <BrowserRouter>
-
-//             <NavBar />
-
-//             <Routes>
-
-//                 <Route path="/" element={<Navigate to="/home" replace />} />
-
-//                 <Route path="/home" element={<Home />} />
-
-//                 <Route
-//                     path="/login"
-//                     element={
-//                         isLoggedIn
-//                             ? <Navigate to="/employees" replace />
-//                             : <Login />
-//                     }
-//                 />
-
-//                 <Route path="/register" element={<Register />} />
-
-//                 <Route
-//                     path="/employees"
-//                     element={
-//                         isLoggedIn
-//                             ? <Parent />
-//                             : <Navigate to="/login" replace />
-//                     }
-//                 />
-
-//                 <Route path="*" element={<Page404 />} />
-
-//             </Routes>
-
-//         </BrowserRouter>
+//         <>
+//             <BrowserRouter>
+//                 <NavBar></NavBar>
+//                 <Routes>
+//                     <Route path="/" element={<Navigate to="/home" replace />} />
+//                     <Route path="/home" element={<Home />} />
+//                     <Route path="/login" element={<Login />} />
+//                     <Route path="/register" element={<Register />} />
+//                     <Route path="/employees" element={<Home />} />
+//                     <Route path="*" element={<Page404 />} />
+//                 </Routes>
+//             </BrowserRouter>
+//         </>
 //     );
+
 // };
 
 // export default AppRoutes;
+
+
 
 
 
