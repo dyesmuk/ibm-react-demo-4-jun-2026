c978e8618b6bb14797e6c8006a3f4143bdb1c5e3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-30 10:46:48 +0530

    initial commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const Login = () => {
 
+    const apiUrl: string = 'https://jsonplaceholder.typicode.com/users/?username=Antonette';
+
     const [user, setUser] = useState({ username: '', password: '' });
 
     const [message, setMessage] = useState('');
@@ -16,6 +18,8 @@ const Login = () => {
 
     const submitInput = (evt) => {
         evt.preventDefault(); // important 
+        // axios here 
+        // https://www.npmjs.com/package/axios 
         if (user.username === 'sonu' && user.password === 'sonu') {
             setUser({ username: '', password: '' });
             setMessage('Login successful!');
@@ -43,6 +47,52 @@ const Login = () => {
     );
 };
 export default Login;
+
+// import { useState } from "react";
+
+// const Login = () => {
+
+//     const [user, setUser] = useState({ username: '', password: '' });
+
+//     const [message, setMessage] = useState('');
+
+//     const handleInput = (evt) => {
+//         const { name, value } = evt.target;
+//         setUser((prevUser) => ({
+//             ...prevUser,
+//             [name]: value,
+//         }));
+//     };
+
+//     const submitInput = (evt) => {
+//         evt.preventDefault(); // important
+//         if (user.username === 'sonu' && user.password === 'sonu') {
+//             setUser({ username: '', password: '' });
+//             setMessage('Login successful!');
+//         }
+//         else {
+//             setUser({ username: '', password: '' });
+//             setMessage('Invalid credentials.');
+//         }
+//     };
+
+//     return (
+//         <>
+//             <h1>Login Component </h1>
+//             <p>This is login component.</p>
+//             <form>
+//                 <input type="text" name="username" value={user.username} onChange={handleInput} autoFocus placeholder="Enter your username" />
+//                 <br />
+//                 <input type="password" name="password" value={user.password} onChange={handleInput} placeholder="Enter your username" />
+//                 <br />
+//                 <input type="submit" onClick={submitInput} />
+//             </form>
+
+//             <p>{message && message}</p>
+//         </>
+//     );
+// };
+// export default Login;
 
 // import { useState } from "react";
 
